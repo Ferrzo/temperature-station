a1573e91cf28ee83a606496d4cb3474eac547453
Jaromir Konupka <jaromirkonupka@Jaromir-MacBook-Pro.local>
2019-02-10 22:51:24 +0100

    cleanup and connect sensor to api

--- index.js
+++ index.js
@@ -8,33 +8,23 @@ firebase.initializeApp({
   databaseURL: "https://home-temperature-station.firebaseio.com"
 });
 
-//TODO: Connect with raspberry PI extensions
-
+let db = firebase.database();
+let ref = db.ref('temperature');
 sensor.readSimpleC((err, temperature) => {
   if(err) {
     console.log(err);
   } else {
-    console.log(temperature);
+    const now = new Date().getTime();
+    const newData = {temperature: temperature, date: now};
+
+    ref.push(newData,
+       (a) => { if (a) { console.log(a); } else {console.log(`logged ${temperature} at ${now}.`) } });
   }
 });
 
-
-
-//TODO: Cron vs timer
-
-//TEST:
-const now = new Date().getTime();
-const temperatureValue = 33.33;
-
-
-let db = firebase.database();
-let ref = db.ref('temperature');
+// GET DATA:
 /*ref.on('value', function(snapshot) {
     console.log(snapshot.val());
   });*/
 
 
-ref.push({
-    temperature: temperatureValue,
-    date: now
-}, (a) => { if (a) { console.log(a); } else {console.log(`logged ${temperatureValue} at ${now}.`) } });
